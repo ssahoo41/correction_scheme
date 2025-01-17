79a26fe7b3538820fcdf07a720000df6fec4c035
Sushree Jagriti Sahoo <ssahoo41@gatech.edu>
2025-01-17 17:22:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     install_requires=[*open('requirements.txt').read().splitlines(),
                       'NNSubsampling @ git++https://github.com/ray38/NNSubsampling.git'],
     python_requires=">=3.9",
-    author="Lisette del Pino & Jagriti Sahoo",
-    author_email=["jsahoo3@gatech.edu", "lpino3@gatech.edu"],
+    author="Lisette del Pino & Sushree Jagriti Sahoo",
+    author_email=["ssahoo41@gatech.edu", "lpino3@gatech.edu"],
     description="A package for molecular correction schemes",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown"
