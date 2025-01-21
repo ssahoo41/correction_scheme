926f7d0e48522b458a7a705c211574603aae47b1
Lisette <delpinolisette@gmail.com>
2025-01-20 21:49:24 -0500

    setup changes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
                       'NNSubsampling @ git++https://github.com/ray38/NNSubsampling.git'],
     python_requires=">=3.9",
     author="Lisette del Pino & Jagriti Sahoo",
-    author_email=["jsahoo3@gatech.edu", "lpino3@gatech.edu"],
+    author_email=["ssahoo41@gatech.edu", "lpino3@gatech.edu"],
     description="A package for molecular correction schemes",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown"
